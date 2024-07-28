40d1ed578238c959cbc685cbeef8006916983425
Novke <novke.brate@gmail.com>
2024-07-28 15:27:38 +0200

    konfigurisana eureka & users

--- EurekaServer/src/main/java/fon/mas/novica/spring/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/fon/mas/novica/spring/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package fon.mas.novica.spring.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- Users/src/main/java/fon/mas/novica/spring/users/UsersApplication.java
+++ Users/src/main/java/fon/mas/novica/spring/users/UsersApplication.java
@@ -10,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +21,7 @@ import java.util.List;
 @SpringBootApplication
 @RequiredArgsConstructor
 @Slf4j
+@EnableDiscoveryClient
 public class UsersApplication {
 
     private final RolesRepository rolesRepository;
