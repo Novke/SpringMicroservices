22d75164e7906234818c587070b33bd43d60cc1f
Novke <novke.brate@gmail.com>
2024-07-28 14:23:30 +0200

    INIT & DB & User, Role entities

---
+++ Users/src/main/java/fon/mas/novica/spring/users/model/entity/Role.java
@@ -0,0 +1,19 @@
+package fon.mas.novica.spring.users.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+}
---
+++ Users/src/main/java/fon/mas/novica/spring/users/model/entity/User.java
@@ -0,0 +1,26 @@
+package fon.mas.novica.spring.users.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @ManyToOne(optional = false)
+    private Role role;
+}
