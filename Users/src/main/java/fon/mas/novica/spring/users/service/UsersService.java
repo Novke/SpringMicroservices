41b6527c5553adfb4639b4bc09dad25d8832595f
Novke <novke.brate@gmail.com>
2024-07-28 15:17:47 +0200

    UserService je interfejs

--- Users/src/main/java/fon/mas/novica/spring/users/service/UsersService.java
+++ Users/src/main/java/fon/mas/novica/spring/users/service/UsersService.java
@@ -2,33 +2,14 @@ package fon.mas.novica.spring.users.service;
 
 import fon.mas.novica.spring.users.model.dto.user.CreateUserCmd;
 import fon.mas.novica.spring.users.model.dto.user.UserInfo;
-import fon.mas.novica.spring.users.model.entity.UserEntity;
-import fon.mas.novica.spring.users.repository.RolesRepository;
-import fon.mas.novica.spring.users.repository.UsersRepository;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class UsersService {
+public interface UsersService {
 
-    private final UsersRepository usersRepository;
-    private final RolesRepository rolesRepository;
-    private final ModelMapper mapper;
 
-    public UserInfo createUser(CreateUserCmd user) {
-        UserEntity userRequest = mapper.map(user, UserEntity.class);
-        userRequest.setRole(rolesRepository.findById(1L).orElseThrow());
-        UserEntity createdUser = usersRepository.save(userRequest);
-        return mapper.map(createdUser, UserInfo.class);
-    }
+    UserInfo createUser(CreateUserCmd user);
 
-    public List<UserInfo> findAllUsers() {
-        return usersRepository.findAll().stream()
-                .map(u -> mapper.map(u, UserInfo.class))
-                .toList();
-    }
+    List<UserInfo> findAllUsers();
 }
---
+++ Users/src/main/java/fon/mas/novica/spring/users/service/impl/UsersServiceImpl.java
@@ -0,0 +1,37 @@
+package fon.mas.novica.spring.users.service.impl;
+
+import fon.mas.novica.spring.users.model.dto.user.CreateUserCmd;
+import fon.mas.novica.spring.users.model.dto.user.UserInfo;
+import fon.mas.novica.spring.users.model.entity.UserEntity;
+import fon.mas.novica.spring.users.repository.RolesRepository;
+import fon.mas.novica.spring.users.repository.UsersRepository;
+import fon.mas.novica.spring.users.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsersServiceImpl implements UsersService {
+
+
+        private final UsersRepository usersRepository;
+        private final RolesRepository rolesRepository;
+        private final ModelMapper mapper;
+
+        public UserInfo createUser(CreateUserCmd user) {
+            UserEntity userRequest = mapper.map(user, UserEntity.class);
+            userRequest.setRole(rolesRepository.findById(1L).orElseThrow());
+            UserEntity createdUser = usersRepository.save(userRequest);
+            return mapper.map(createdUser, UserInfo.class);
+        }
+
+        public List<UserInfo> findAllUsers() {
+            return usersRepository.findAll().stream()
+                    .map(u -> mapper.map(u, UserInfo.class))
+                    .toList();
+    }
+
+}
