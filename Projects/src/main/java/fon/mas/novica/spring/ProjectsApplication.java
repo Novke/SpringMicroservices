3dabc1ab7b899cb859763c8fc56bf98f16e57d47
Novke <novke.brate@gmail.com>
2024-07-30 18:29:59 +0200

    projects config za configServer i eureku

--- Projects/src/main/java/fon/mas/novica/spring/ProjectsApplication.java
+++ Projects/src/main/java/fon/mas/novica/spring/ProjectsApplication.java
@@ -1,13 +1,33 @@
 package fon.mas.novica.spring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
 public class ProjectsApplication {
+
+    @Value("${config.source}")
+    String configSource;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectsApplication.class, args);
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    void configServerEchoTest(){
+        log.info("======== REACHING CONFIG SERVER PROPERTIES ========");
+        if ("Local".equalsIgnoreCase(configSource)) {
+            log.warn("Config source is Local!");
+        } else {
+            log.info("Config source: " + configSource);
+        }
+        log.info("===================================================");
     }
 
 }
